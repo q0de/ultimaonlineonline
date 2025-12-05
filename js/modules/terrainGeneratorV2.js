0c4994cf7f62c1511bebb277f7c146632a3dadb4
q0de <michaelagrande@gmail.com>
2025-12-05 12:16:08 -0600

    Force module reload for Grey Rock pattern fix with version parameter

--- js/modules/terrainGeneratorV2.js
+++ js/modules/terrainGeneratorV2.js
@@ -21,7 +21,7 @@ import {
     getDarkForestTile,
     getMountainRockTile,
     BIOME_VARIANT_WEIGHTS
-} from '../data/uoTileSetsClean.js';
+} from '../data/uoTileSetsClean.js?v=greyrock_fix';
 import { 
     SAND_TO_GRASS_TRANSITIONS,
     GRASS_TO_SAND_TRANSITIONS, 
